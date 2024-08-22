6a4986de6f50bba0441429acaba57e1c59ad5570
Brian <thinhle1199@gmail.com>
2024-08-21 23:01:08 -0500

    submit

--- src/MergeStringsAlternately1768.java
+++ src/MergeStringsAlternately1768.java
@@ -1,5 +1,15 @@
 public class MergeStringsAlternately1768 {
 
+    /*
+     * Given: Two string word1, word2
+     * 
+     * Goal: Merge two string into one, by alternately (luan phien) adding characters 
+     * starting from word1. If one string is longer, append addictional characters too
+     * 
+     * Time Complexity: O(m+n) m is length of word1, n is length of word2
+     * Space Complexity: O(m+n) 
+     */
+
     public static String mergeAlternately(String word1, String word2) {
         StringBuilder result = new StringBuilder();
         int i = 0;
