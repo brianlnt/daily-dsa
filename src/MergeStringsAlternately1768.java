6367a0a67667b68372356ccff23aa49271727d29
Brian <thinhle1199@gmail.com>
2024-08-21 20:35:09 -0500

    submit

--- src/MergeStringsAlternately1768.java
+++ src/MergeStringsAlternately1768.java
@@ -1,5 +1,23 @@
 public class MergeStringsAlternately1768 {
+
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while(i < word1.length() || i < word2.length()){
+            if(i < word1.length()){
+                result.append(word1.charAt(i));
+            }
+            if(i < word2.length()){
+                result.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return result.toString();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(mergeAlternately("abc", "pqr"));
+        System.out.println(mergeAlternately("ab", "pqrs"));
+        System.out.println(mergeAlternately("abcd", "pq"));
     }
 }
