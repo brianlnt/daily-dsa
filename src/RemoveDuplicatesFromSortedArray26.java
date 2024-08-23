18f6e876faa9fd194372adfa224307aebfb494b3
Brian <thinhle1199@gmail.com>
2024-08-23 18:41:14 -0500

    submit

--- src/RemoveDuplicatesFromSortedArray26.java
+++ src/RemoveDuplicatesFromSortedArray26.java
@@ -1,11 +1,13 @@
 public class RemoveDuplicatesFromSortedArray26{
+    /*
+     * Time: O(n)
+     * Space: O(1)
+     */
     public static int removeDuplicates(int[] nums) {
+        if(nums.length == 0) return 0;
         int insertIndex = 1;
         for (int i = 1; i < nums.length; i++) {
-            // We skip to next index if we see a duplicate element
             if (nums[i - 1] != nums[i]) {
-                /* Storing the unique element at insertIndex index and incrementing
-                   the insertIndex by 1 */
                 nums[insertIndex] = nums[i];
                 insertIndex++;
             }
