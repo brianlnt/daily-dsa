419c740ea008d06f04201210bdcc855d8ce796fd
Brian <thinhle1199@gmail.com>
2024-08-24 08:59:24 -0500

    submit

--- src/MergeSortedArray88.java
+++ src/MergeSortedArray88.java
@@ -7,11 +7,34 @@ public class MergeSortedArray88 {
      * Space: O(n)
      */
     public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[i + m] = nums2[i];
+        /* Brute Force */
+        // for (int i = 0; i < n; i++) {
+        //     nums1[i + m] = nums2[i];
+        // }
+        // Arrays.sort(nums1);
+        // return nums1;
+
+        /* Three pointers start from beginning */
+        int[] nums1Copy = new int[m];
+        for(int i = 0; i < nums1Copy.length; i++){
+            nums1Copy[i] = nums1[i];
         }
-        Arrays.sort(nums1);
+
+        int p1 = 0;
+        int p2 = 0;
+        int p = 0;
+        while(p < nums1.length){
+            if(p2 >= n || (p1 < m && nums1Copy[p1] <= nums2[p2])){
+                nums1[p] = nums1Copy[p1];
+                p1++;
+            }else{
+                nums1[p] = nums2[p2];
+                p2++;
+            }
+            p++;
+        }
         return nums1;
+
     }
     public static void main(String[] args){
         System.out.println(Arrays.toString(merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3)));
