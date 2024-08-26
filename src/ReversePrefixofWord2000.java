49d00d44b08e7793279c2fc155459f3ca0e8d45c
Brian <thinhle1199@gmail.com>
2024-08-25 22:54:50 -0500

    submit

--- src/ReversePrefixofWord2000.java
+++ src/ReversePrefixofWord2000.java
@@ -5,16 +5,30 @@ public class ReversePrefixofWord2000 {
      * Space: O(n)
      */
     public static String reversePrefix(String word, char ch) {
-        char[] result = word.toCharArray();
+        // char[] result = word.toCharArray();
+        // for(int i = 0; i < word.length(); i++){
+        //     if(result[i] == ch){
+        //         for(int j = i; j >= 0; j--){
+        //             result[i-j] = word.charAt(j);
+        //         }
+        //         return new String(result);
+        //     }
+        // }
+        // return word;
+        int chIndex = word.indexOf(ch);
+        if(chIndex == -1) return word;
+
+        StringBuilder result = new StringBuilder();
+
         for(int i = 0; i < word.length(); i++){
-            if(result[i] == ch){
-                for(int j = i; j >= 0; j--){
-                    result[i-j] = word.charAt(j);
-                }
-                return new String(result);
+            if(i <= chIndex){
+                result.append(word.charAt(chIndex - i));
+            } else {
+                result.append(word.charAt(i));
             }
         }
-        return word;
+
+        return result.toString();
     }
 
     public static void main(String[] args) {
