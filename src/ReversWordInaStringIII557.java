4dcbabdd3b3186eee04fd81ae78d988fc2599ed0
Brian <thinhle1199@gmail.com>
2024-09-11 16:59:48 -0500

    submit 557

---
+++ src/ReversWordInaStringIII557.java
@@ -0,0 +1,28 @@
+public class ReversWordInaStringIII557 {
+    public static String reverseWords(String s) {
+        char[] sChar = s.toCharArray();
+        int lastSpaceIndex = -1;
+
+        for(int i = 0 ; i <= sChar.length; i++){
+            if(i == sChar.length || sChar[i] == ' '){
+                int start = lastSpaceIndex + 1;
+                int end = i - 1;
+            
+                while(start < end){
+                    char temp = sChar[start];
+                    sChar[start] = sChar[end];
+                    sChar[end] = temp;
+                    start++;
+                    end--;
+                }
+                lastSpaceIndex = i;
+            }
+        }
+        return new String(sChar);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Let's take LeetCode contest"));
+        System.out.println(reverseWords("Mr Ding"));
+    }
+}
