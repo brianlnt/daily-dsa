af3b1d9f9c7fbe3a1242e5dda4185343108bb4be
Brian <thinhle1199@gmail.com>
2024-09-15 21:36:54 -0500

    improve RemovePalindromicSubsequences 1332

--- src/RemovePalindromicSubsequences1332.java
+++ src/RemovePalindromicSubsequences1332.java
@@ -1,5 +1,9 @@
 public class RemovePalindromicSubsequences1332 {
     public static int removePalindromeSub(String s) {
+        /*
+         * Time: O(n)
+         * Space: O(n)
+         */
         if (s.isEmpty()) {
             return 0;
         }
@@ -13,6 +17,32 @@ public class RemovePalindromicSubsequences1332 {
         return 2;
     }
 
+    public static int removePalindromeSub1(String s) {
+        /*
+         * Time: O(n)
+         * Space: O(n)
+         */
+        
+        if(s.isEmpty()) return 0;
+        
+        if(isPalindrome(s)){
+            return 1;
+        }
+        return 2;
+    }
+
+    private static boolean isPalindrome(String s){
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end)) return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
         System.out.println(removePalindromeSub("ababa"));
         System.out.println(removePalindromeSub("abb"));
