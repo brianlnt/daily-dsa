ddd76ea22f6b753c1549c260d612cc1e9e1cbdff
Brian <thinhle1199@gmail.com>
2024-09-16 21:32:05 -0500

    improve Duplicate Zero 1089

--- src/DuplicateZore1089.java
+++ src/DuplicateZore1089.java
@@ -28,10 +28,54 @@ public class DuplicateZore1089 {
         return arr;
     }
 
+    public static int[] duplicateZeros1(int[] arr) {
+        /*
+         * Time: O(n)
+         * Space: O(1)
+         */
+        int possibleDups = 0;
+        int length_ = arr.length - 1;
+
+        // Find the number of zeros to be duplicated
+        // Stopping when left points beyond the last element in the original array
+        // which would be part of the modified array
+        for (int left = 0; left <= length_ - possibleDups; left++) {
+
+            // Count the zeros
+            if (arr[left] == 0) {
+
+                // Edge case: This zero can't be duplicated. We have no more space,
+                // as left is pointing to the last element which could be included  
+                if (left == length_ - possibleDups) {
+                    // For this zero we just copy it without duplication.
+                    arr[length_] = 0;
+                    length_ -= 1;
+                    break;
+                }
+                possibleDups++;
+            }
+        }
+
+        // Start backwards from the last element which would be part of new array.
+        int last = length_ - possibleDups;
+
+        // Copy zero twice, and non zero once.
+        for (int i = last; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possibleDups] = 0;
+                possibleDups--;
+                arr[i + possibleDups] = 0;
+            } else {
+                arr[i + possibleDups] = arr[i];
+            }
+        }
+
+        return arr;
+    }
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(duplicateZeros(new int[]{1,0,2,3,0,4,5,0})));
-        System.out.println(Arrays.toString(duplicateZeros(new int[]{1,2,3})));
-        System.out.println(Arrays.toString(duplicateZeros(new int[]{0,0,0,0,0,0,0,0})));
+        System.out.println(Arrays.toString(duplicateZeros1(new int[]{1,0,2,3,0,4,5,0})));
+        System.out.println(Arrays.toString(duplicateZeros1(new int[]{1,2,3})));
+        System.out.println(Arrays.toString(duplicateZeros1(new int[]{0,0,0,0,0,0,0,0})));
     }
 
 
