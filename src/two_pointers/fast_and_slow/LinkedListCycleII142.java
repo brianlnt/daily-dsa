c11f51f628d691269b453c339114fb1d8d952a1c
Brian <thinhle1199@gmail.com>
2024-09-22 23:14:31 -0500

    improve space complexity #142: Linked List Cycle II

--- src/two_pointers/fast_and_slow/LinkedListCycleII142.java
+++ src/two_pointers/fast_and_slow/LinkedListCycleII142.java
@@ -22,6 +22,35 @@ public class LinkedListCycleII142 {
          return null;
     }
 
+    public static ListNode detectCycle1(ListNode head) {
+        /*
+        Time: O(n)
+        Space: O(1)
+        */
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                break;
+            }
+        }
+
+        if(fast == null || fast.next == null) return null;
+
+        fast = head;
+
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(3);
         linkedList.append(2);
@@ -30,5 +59,6 @@ public class LinkedListCycleII142 {
         linkedList.cycle(1);
 
         System.out.println(detectCycle(linkedList.head).val);
+        System.out.println(detectCycle1(linkedList.head).val);
     }
 }
