a333b46677df592d3b490ef2cc87df0a651fc66e
Brian <thinhle1199@gmail.com>
2024-09-30 13:34:36 -0500

    W1D1: P1

--- untitled/src/GCD.java
+++ untitled/src/GCD.java
@@ -1,10 +1,28 @@
 public class GCD {
     public static int gcd(int a, int b) {
+        /*
+        P problem
+        Time: O(log(n)) ~ O(n)
+         */
         if(b == 0) return a;
         return gcd(b, a % b);
     }
 
+    public static int gcd1(int a, int b) {
+        /*
+        Iterative solution
+        Time: O(n)
+         */
+        while(b != 0){
+            int temp = b;
+            b = a % temp;
+            a = temp;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
-        System.out.println(gcd(16, 36));
+        System.out.println(gcd(36, 16));
+        System.out.println(gcd1(36, 16));
     }
 }
