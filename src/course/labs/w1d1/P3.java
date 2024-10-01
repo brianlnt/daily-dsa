b348ce32086b1e5d8e24198f47a1d294196f9458
Brian <thinhle1199@gmail.com>
2024-09-30 23:44:05 -0500

    add another solution #242: Valid Anagram

--- src/course/labs/w1d1/P3.java
+++ src/course/labs/w1d1/P3.java
@@ -6,6 +6,11 @@ public class P3 {
     /*
     242. Valid Anagram (https://leetcode.com/problems/valid-anagram/description/)
      */
+
+    /*
+    Time: O(nlogn)
+    Space: O(n)
+     */
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         char[] charArray1 = s.toCharArray();
@@ -15,5 +20,26 @@ public class P3 {
         Arrays.sort(charArray2);
 
         return Arrays.equals(charArray1, charArray2);
+    }
+
+    /*
+    Time: O(n)
+    Space: O(n)
+     */
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
