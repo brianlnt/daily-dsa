f4c6ab6d8946aae17ba12db7c9bd294bb2262398
Brian <thinhle1199@gmail.com>
2024-09-30 21:57:58 -0500

    submit #206: Reverse Linked List

---
+++ src/linked_list/Main.java
@@ -0,0 +1,15 @@
+package linked_list;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(3);
+        linkedList.append(1);
+        linkedList.append(2);
+        linkedList.append(5);
+        linkedList.append(10);
+        linkedList.append(15);
+        linkedList.print();
+        linkedList.reverse();
+        linkedList.print();
+    }
+}
