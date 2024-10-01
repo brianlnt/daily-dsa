3ca39661f82cd94232618df7501a02445b6c56a2
Brian <thinhle1199@gmail.com>
2024-09-30 23:37:43 -0500

    submit #242: Valid Anagram

--- src/course/labs/w1d1/P3.java
+++ src/course/labs/w1d1/P3.java
@@ -1,4 +1,19 @@
 package course.labs.w1d1;
 
+import java.util.Arrays;
+
 public class P3 {
+    /*
+    242. Valid Anagram (https://leetcode.com/problems/valid-anagram/description/)
+     */
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
 }
