43f99d56e9fd69bc6670e3e6413d4565e0ccda43
Brian <thinhle1199@gmail.com>
2024-10-02 22:32:25 -0500

    Approach 1: Min heap - #414: Third Max Number

--- src/technique/top_k_element/ThirdMaxNumber414.java
+++ src/technique/top_k_element/ThirdMaxNumber414.java
@@ -1,6 +1,7 @@
 package technique.top_k_element;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class ThirdMaxNumber414 {
     /*
@@ -37,14 +38,37 @@ public class ThirdMaxNumber414 {
         Approach 2: Min-heap to find kth largest element
          */
 
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
+        for(int i = 0; i < nums.length; i++){
+            if(!minHeap.contains(nums[i])){
+                minHeap.add(nums[i]);
+                if(minHeap.size() > 3){
+                    minHeap.remove();
+                }
+            }else{
+                continue;
+            }
+        }
 
-        return 1;
+        if(minHeap.size() == 2) {
+            int firstNum = minHeap.remove();
+            // int firstNum = minHeap.poll(); poll: retrieve and remove , peek: retrieve only
+            return Math.max(firstNum, minHeap.peek());
+        }
+        
+        return minHeap.peek();
+
+        /*
+         * Time: 0(3)
+         * Space: O(3)
+         */
     }
 
     public static void main(String[] args) {
-        System.out.println(thirdMax1(new int[]{4,4,2,5,3,1}));
-        System.out.println(thirdMax1(new int[]{2,2,3,1}));
+        System.out.println(thirdMax2(new int[]{4,4,2,5,3,1}));
+        System.out.println(thirdMax2(new int[]{2,2,3,1}));
+        System.out.println(thirdMax2(new int[]{3,1}));
     }
 
 }
