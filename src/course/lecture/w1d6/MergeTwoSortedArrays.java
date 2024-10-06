a0857fa02eb25079d142a38aecffd14aa1705854
Brian <thinhle1199@gmail.com>
2024-10-05 22:16:10 -0500

    w1d6: Merge two sorted array (use extra space)

--- src/course/lecture/w1d6/MergeTwoSortedArrays.java
+++ src/course/lecture/w1d6/MergeTwoSortedArrays.java
@@ -37,6 +37,42 @@ public class MergeTwoSortedArrays {
          */
     }
 
+    public static int[] merge2(int[] a, int[] b, int n, int m){
+        int i = n - 1;
+        int j = m - 1;
+        int p = m+n-1;
+        int[] mergeResult = new int[m+n];
+
+        while(i > -1 && j > -1 && p > -1){
+            if(a[i] > b[j]){
+                mergeResult[p] = a[i--];
+            } else {
+                mergeResult[p] = b[j--];
+            }
+            p--;
+        }
+
+        //add remaining elements from A
+        while(i > -1){
+            mergeResult[p--] = a[i--];
+        }
+
+        //add remaining elements from B
+        while(j > -1){
+            mergeResult[p--] = b[j--];
+        }
+
+        for(int k = 0; k < p; k++){
+            a[k] = mergeResult[k];
+        }
+
+        return a;
+        /*
+        Time: O(m+n)
+        Space: O(1)
+         */
+    }
+
     public static void main(String[] args) {
         int[] A = {5, 8, 10, 15, 0, 0, 0};
         int n = 4;
@@ -44,6 +80,6 @@ public class MergeTwoSortedArrays {
         int m = 3;
 
         System.out.println(Arrays.toString(merge1(A,B,n,m)));
-//        System.out.println(Arrays.toString(merge2(A,B,n,m)));
+        System.out.println(Arrays.toString(merge2(A,B,n,m)));
     }
 }
